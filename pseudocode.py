e18803543c19b9c2ce2c3dcaceadcb15296c2419
Travis Hammond <dashdeckers@gmail.com>
2019-04-07 18:46:28 +0200

    The code works, now just a final optimization is needed to compute similarities without trying to pass the entire matrix to the cosine function at once (gives a MemoryError). Total time taken to finish normalization: 765 seconds

--- pseudocode.py
+++ pseudocode.py
@@ -12,32 +12,50 @@ matrix = np.full((num_users, num_shows), -1, dtype=int)
 
 # read data, and fill in the utiliy matrix
 # watched but not rated is treated the same as not watched
-with open('rating.csv') as csv_file:
+with open('data/rating.csv') as csv_file:
     ratings = csv.reader(csv_file, delimiter=',')
+    next(ratings, None) # skip the header
+    # anime show ids dont correspond to idx numbers, they are unique
+    # id numbers from the website itself, so we have to map them to 
+    # indices with a dictionary. To find the anime_id of col_idx, do
+    # anime_id = anime_id_mappings[col_idx]
+    anime_id_mappings = dict()
+    show_number = 0
     for idx, row in enumerate(ratings):
-        user_id, anime_id, rating = row
-        matrix[user_id][anime_id] = rating
+        user_id, anime_id, rating = map(int, row)
+        if anime_id in anime_id_mappings:
+            anime_id = anime_id_mappings[anime_id]
+        else:
+            anime_id_mappings[anime_id] = show_number
+            anime_id = show_number
+            show_number += 1
+        matrix[user_id - 1][anime_id - 1] = rating
 
-print(f"Finished reading the data... (after {t0-time.time()})")
+print(f"Finished reading the data... (after {time.time()-t0})")
 
 # checks if the value is a valid rating
 def is_rating(value):
     return not value == -1
 
 # normalize ratings for each row in utility matrix
+num_users_with_no_ratings = 0
 for row_idx in range(num_users):
     # only valid ratings should be used to calculate avg_rating
-    ratings = filter(is_rating, matrix[row_idx])
-    avg_rating = sum(ratings) / len(ratings)
-    # for each element in the matrix row, subtract the average
-    # rating. if it is not a valid rating, set the value to 0
-    for col_idx in range(num_shows):
-        if is_rating(matrix[row_idx][col_idx]):
-            matrix[row_idx][col_idx] -= avg_rating
-        else:
-            matrix[row_idx][col_idx] == 0
+    ratings = list(filter(is_rating, matrix[row_idx]))
+    if len(ratings) > 0:
+        avg_rating = sum(ratings) / len(ratings)
+        # for each element in the matrix row, subtract the average
+        # rating. if it is not a valid rating, set the value to 0
+        for col_idx in range(num_shows):
+            if is_rating(matrix[row_idx][col_idx]):
+                matrix[row_idx][col_idx] -= avg_rating
+            else:
+                matrix[row_idx][col_idx] == 0
+    else:
+        num_users_with_no_ratings += 1
+print(f"Number of users who did not rate anything: {num_users_with_no_ratings}")
 
-print(f"Finished normalizing the data... (after {t0-time.time()})")
+print(f"Finished normalizing the data... (after {time.time()-t0})")
 
 # this calculates the similarity of each row with each row,
 # resulting in a matrix of shape (num_users, num_users) where
@@ -47,7 +65,7 @@ print(f"Finished normalizing the data... (after {t0-time.time()})")
 # the similarity between that user and itself)
 cos_sims = cosine(matrix)
 
-print(f"Finished computing similarities... (after {t0-time.time()})")
+print(f"Finished computing similarities... (after {time.time()-t0})")
 
 np.save('utility_matrix', matrix)
 np.save('similarities', cos_sims)
